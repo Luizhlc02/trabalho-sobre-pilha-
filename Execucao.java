3635f7f9f549df2575cc9108c05ab1fe99e7f570
Luiz Henrique <luizhlc02@gmail.com>
2023-05-16 13:09:18 -0300

    questao 3

--- Execucao.java
+++ Execucao.java
@@ -1,10 +1,14 @@
 package Pilha;
 
-public class Execucao {
+public class Execucao extends generica{
 
 	public static void main(String[] args) {
+		Pilha<Integer> pilha = new Pilha<>();
+		pilha.push(1);
+		pilha.push(2);
 		
-
+		System.out.println(pilha.top()); 
+		
 	}
 
 }
