80c3bd93e45ae616549eebbac08be69563995b50
Luiz Henrique <luizhlc02@gmail.com>
2023-05-16 22:19:40 -0300

    .

--- Execucao.java
+++ Execucao.java
@@ -1,13 +1,11 @@
 package Pilha;
 
-public class Execucao extends generica{
+public class Execucao{
 
 	public static void main(String[] args) {
-		Pilha<Integer> pilha = new Pilha<>();
-		pilha.push(1);
-		pilha.push(2);
 		
-		System.out.println(pilha.top()); 
+		
+		
 		
 	}
 
--- Notas.java
+++ Notas.java
@@ -1,6 +1,41 @@
 package Pilha;
 
-public class Notas {
+import java.util.Scanner;
+import java.util.Stack;
 
+public class Notas {
+	public static void main(String[]args) {
+		Stack<String> pilhaNotas = new Stack<>();
+		Scanner sc = new Scanner (System.in);
+		while(true) {
+		System.out.println("Digite a nota:  ");
+		String notas = sc.next();
+		
+		
+		
+		
 	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+	}
+	
+}
 }
